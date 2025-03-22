d95aa62d594c4a4af777095ea257438c26026378
Rafael Aguiar <rafael.a1@hotmail.com>
2025-03-22 13:25:05 -0300

    feat: atualizar inteface do TaskRepository para atualizar tarefa

--- backend/modules/tasks/data/ITasksRepository.ts
+++ backend/modules/tasks/data/ITasksRepository.ts
@@ -1,8 +1,10 @@
 import { Task } from "@prisma/client";
 import { ICreateTaskDTO } from "../dtos/ICreateTaskDTO";
+import { IUpdateTaskDTO } from "../dtos/IUpdateTaskDTO";
 
 interface ITasksRepository {
     create(data: ICreateTaskDTO): Promise<Task>
+    updateById(id: number, updates: IUpdateTaskDTO): Promise<void>
 }
 
 export { ITasksRepository }
