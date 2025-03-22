ab3760f6b1b8952bbd9a8a0cecf2cdb8e3840cf7
Rafael Aguiar <rafael.a1@hotmail.com>
2025-03-22 13:44:25 -0300

    feat: adicionar rota get /tasks para listar as tarefas

--- backend/shared/infra/http/routes/tasks.routes.ts
+++ backend/shared/infra/http/routes/tasks.routes.ts
@@ -1,12 +1,15 @@
 import { Router } from "express"
 import { CreateTaskController } from "../../../../modules/tasks/controllers/CreateTaskController"
 import { UpdateTaskController } from "../../../../modules/tasks/controllers/UpdateTaskController"
+import { ListTaskController } from "../../../../modules/tasks/controllers/ListTaskController"
 
 const tasksRoutes = Router()
 
 const createTaskController = new CreateTaskController()
 const updateTaskController = new UpdateTaskController()
+const listTaskController = new ListTaskController()
 
+tasksRoutes.get('/', listTaskController.handle.bind(listTaskController))
 tasksRoutes.post('/', createTaskController.handle.bind(createTaskController))
 tasksRoutes.put('/', updateTaskController.handle.bind(createTaskController))
 
