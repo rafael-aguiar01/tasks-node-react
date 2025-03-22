875ba36ccaa1ea296b8c0ef3099a7c311e217f46
Rafael Aguiar <rafael.a1@hotmail.com>
2025-03-22 13:55:13 -0300

    feat: adicionar rota delete /tasks para deletar uma tarefa

--- backend/shared/infra/http/routes/tasks.routes.ts
+++ backend/shared/infra/http/routes/tasks.routes.ts
@@ -2,16 +2,19 @@ import { Router } from "express"
 import { CreateTaskController } from "../../../../modules/tasks/controllers/CreateTaskController"
 import { UpdateTaskController } from "../../../../modules/tasks/controllers/UpdateTaskController"
 import { ListTaskController } from "../../../../modules/tasks/controllers/ListTaskController"
+import { DeleteTaskController } from "../../../../modules/tasks/controllers/DeleteTaskController"
 
 const tasksRoutes = Router()
 
 const createTaskController = new CreateTaskController()
 const updateTaskController = new UpdateTaskController()
 const listTaskController = new ListTaskController()
+const deleteTaskController = new DeleteTaskController()
 
 tasksRoutes.get('/', listTaskController.handle.bind(listTaskController))
 tasksRoutes.post('/', createTaskController.handle.bind(createTaskController))
 tasksRoutes.put('/', updateTaskController.handle.bind(createTaskController))
+tasksRoutes.delete('/:id', deleteTaskController.handle.bind(deleteTaskController))
 
 
 export { tasksRoutes }
