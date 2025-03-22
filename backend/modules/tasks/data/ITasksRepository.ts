c51798ad2c5be067ffee7f6a39a488cd01c21532
Rafael Aguiar <rafael.a1@hotmail.com>
2025-03-22 11:38:57 -0300

    refactor: correção de erro no tipo do data do tasksRepository

--- backend/modules/tasks/data/ITasksRepository.ts
+++ backend/modules/tasks/data/ITasksRepository.ts
@@ -1,4 +1,5 @@
 import { Task } from "@prisma/client";
+import { ICreateTaskDTO } from "../dtos/ICreateTaskDTO";
 
 interface ITasksRepository {
     create(data: ICreateTaskDTO): Promise<Task>
--- backend/modules/tasks/infra/prisma/repositories/TasksRepository.ts
+++ backend/modules/tasks/infra/prisma/repositories/TasksRepository.ts
@@ -1,12 +1,17 @@
 import { PrismaClient, Task } from "@prisma/client";
 import { ITasksRepository } from "../../../data/ITasksRepository";
+import { ICreateTaskDTO } from "../../../dtos/ICreateTaskDTO";
 
 const prisma = new PrismaClient()
 
 class TasksRepository implements ITasksRepository {
     async create(data: ICreateTaskDTO): Promise<Task> {
         const task = await prisma.task.create({
-            data,
+            data: {
+                title: data.title, 
+                description: data.description ?? '',
+                status: data.status ?? 'PENDING',
+            }
         })
 
         return task
