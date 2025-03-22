e25f50c43d777c3098c0ff94f6443f0522069c77
Rafael Aguiar <rafael.a1@hotmail.com>
2025-03-22 14:10:18 -0300

    feat: adicionar rota path /tasks para marcar uma tarefa como concluída

--- backend/shared/infra/http/routes/tasks.routes.ts
+++ backend/shared/infra/http/routes/tasks.routes.ts
@@ -3,18 +3,20 @@ import { CreateTaskController } from "../../../../modules/tasks/controllers/Crea
 import { UpdateTaskController } from "../../../../modules/tasks/controllers/UpdateTaskController"
 import { ListTaskController } from "../../../../modules/tasks/controllers/ListTaskController"
 import { DeleteTaskController } from "../../../../modules/tasks/controllers/DeleteTaskController"
+import { UpdateStatusTaskController } from "../../../../modules/tasks/controllers/UpdateStatusTaskController"
 
 const tasksRoutes = Router()
 
 const createTaskController = new CreateTaskController()
 const updateTaskController = new UpdateTaskController()
+const updateTaskStatusController = new UpdateStatusTaskController()
 const listTaskController = new ListTaskController()
 const deleteTaskController = new DeleteTaskController()
 
 tasksRoutes.get('/', listTaskController.handle.bind(listTaskController))
 tasksRoutes.post('/', createTaskController.handle.bind(createTaskController))
 tasksRoutes.put('/:id', updateTaskController.handle.bind(createTaskController))
+tasksRoutes.patch('/:id', updateTaskStatusController.handle.bind(updateTaskStatusController))
 tasksRoutes.delete('/:id', deleteTaskController.handle.bind(deleteTaskController))
-
 
 export { tasksRoutes }
