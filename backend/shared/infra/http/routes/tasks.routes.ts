34943170f95d2082493514e03c1bb35413e29b10
Rafael Aguiar <rafael.a1@hotmail.com>
2025-03-22 13:30:04 -0300

    feat: adicionar rota PUT /tasks para atualização das tarefas

--- backend/shared/infra/http/routes/tasks.routes.ts
+++ backend/shared/infra/http/routes/tasks.routes.ts
@@ -1,10 +1,14 @@
 import { Router } from "express"
 import { CreateTaskController } from "../../../../modules/tasks/controllers/CreateTaskController"
+import { UpdateTaskController } from "../../../../modules/tasks/controllers/UpdateTaskController"
 
 const tasksRoutes = Router()
 
 const createTaskController = new CreateTaskController()
+const updateTaskController = new UpdateTaskController()
 
 tasksRoutes.post('/', createTaskController.handle.bind(createTaskController))
+tasksRoutes.put('/', updateTaskController.handle.bind(createTaskController))
+
 
 export { tasksRoutes }
