bfab6ebc3a4454a1d1224a3a73f146fb1bdab3cb
Rafael Aguiar <rafael.a1@hotmail.com>
2025-03-22 14:09:50 -0300

    feat: criação do controller para marcar tarefas como concluídas

---
+++ backend/modules/tasks/controllers/UpdateStatusTaskController.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from "express";
+import { container } from "tsyringe";
+import { UpdateStatusTaskUseCase } from "../domain/UpdateStatusTaskUseCase";
+
+class UpdateStatusTaskController {
+
+    async handle(request: Request, response: Response): Promise<Response> {
+        try {
+            const id = Number(request.params.id)
+
+            if (isNaN(id)) {
+                return response.status(400).json({ error: "ID inválido" });
+            }
+
+            const updateStatusTaskUseCase = container.resolve(UpdateStatusTaskUseCase);
+            await updateStatusTaskUseCase.execute(id);
+            return response.status(201).send();
+        } catch (error) {
+            return response.status(400).json({ error: error.errors });
+        }
+    }
+}
+
+export { UpdateStatusTaskController }
