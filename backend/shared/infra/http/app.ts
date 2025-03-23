ddfc2a5dd117377c61ecadb968ae493742ed051e
Rafael Aguiar <rafael.a1@hotmail.com>
2025-03-22 21:43:03 -0300

    feat: instalação e configuração do cors

--- backend/shared/infra/http/app.ts
+++ backend/shared/infra/http/app.ts
@@ -1,8 +1,27 @@
 import express from 'express';
 import { router } from './routes';
+import cors from 'cors'
+
 
 const app = express();
 
+const allowedOrigins = [
+    'http://localhost:5173',
+  ]
+
+app.use(cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true)
+      } else {
+        callback(new Error('Not allowed by CORS'))
+      }
+    },
+    allowedHeaders: ['Authorization', 'authorization', 'Content-Type'],
+    methods: ['GET', 'PATCH', 'PUT', 'POST', 'DELETE', 'OPTIONS']
+  }))
+
+  
 app.use(express.json());
 app.use(router)
 
