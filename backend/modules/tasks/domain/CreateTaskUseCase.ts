736d7f8fffe62d2e0376b6050c62f00d2c26f183
Rafael Aguiar <rafael.a1@hotmail.com>
2025-03-22 21:44:31 -0300

    refactor: colocar a tarefa criada para retornar no momento da criação

--- backend/modules/tasks/domain/CreateTaskUseCase.ts
+++ backend/modules/tasks/domain/CreateTaskUseCase.ts
@@ -1,6 +1,7 @@
 import { inject, injectable } from "tsyringe";
 import { ITasksRepository } from "../data/ITasksRepository";
 import { ICreateTaskDTO } from "../dtos/ICreateTaskDTO";
+import { Task } from "@prisma/client";
 
 @injectable()
 class CreateTaskUserCase {
@@ -13,8 +14,9 @@ class CreateTaskUserCase {
         title,
         description,
         status
-    }: ICreateTaskDTO): Promise<void> {
-        await this.tasksRepository.create({title, description, status})
+    }: ICreateTaskDTO): Promise<Task> {
+        const task = await this.tasksRepository.create({title, description, status})
+        return task
     }
 }
 
